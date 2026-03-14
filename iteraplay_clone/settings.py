45bbd3455b2c7ad36fb79351c3b26dda8ada5063
User <user@example.com>
2026-03-14 14:57:44 +0530

    Fixed ALLOWED_HOSTS for Render deployment

--- iteraplay_clone/settings.py
+++ iteraplay_clone/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+f5^ou25c_0679)g0$b5n-qofgo&47jfenn1a)f%gl#xhv8hz7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['iteraplay-1fmv.onrender.com']
 
 
 # Application definition
