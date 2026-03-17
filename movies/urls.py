e5d86cfcea9935302b3705f5ea097a3beecb2193
User <user@example.com>
2026-03-17 12:30:43 +0530

    Add missing movie_links view and url routing

--- movies/urls.py
+++ movies/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.movie_list, name='movies'),
     path('category/<int:category_id>/', views.category_movies, name='category_movies'),
+    path('links/<int:movie_id>/', views.movie_links, name='movie_links'),
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from .models import Movie, Category
 
 def movie_list(request):
@@ -18,3 +19,8 @@ def category_movies(request, category_id):
         'movies': movies,
         'categories': categories
     })
+
+@login_required(login_url='login')
+def movie_links(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'movies/movie_links.html', {'movie': movie})
